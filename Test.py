31160b49f047e198b4ef28fbab348f7710fc54e2
Griffin Malm <griffin.malm@duke.edu>
2018-10-14 10:13:49 -0400

    created function

--- Test.py
+++ Test.py
@@ -20,24 +20,25 @@ def explicit():
 
 explicit()"""
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+def test(req,res):
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources/wakeupcat.jpg')
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'resources/wakeupcat.jpg')
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
 
-image = types.Image(content=content)
+    image = types.Image(content=content)
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+    print('Labels:')
+    for label in labels:
+        print(label.description)
